dee89c7262339fe733adb2983c0c4efcc91f8dae
Ivan Silva <silvaivan280@gmail.com>
2024-04-23 16:16:06 -0300

    cree la clase Producto

---
+++ js/administrador.js
@@ -0,0 +1,1 @@
+import Producto from "./classProducto.js";
---
+++ js/classProducto.js
@@ -0,0 +1,48 @@
+export default class Producto{
+    #codigo;
+    #nombre;
+    #categoria;
+    #precio;
+    constructor(codigo, nombre, categoria, precio){
+        this.#codigo = codigo;
+        this.#nombre = nombre;
+        this.#categoria = categoria;
+        this.#precio = precio;
+    }
+    get codigo(){
+        return this.#codigo;
+    }
+    set codigo(nuevoCodigo){
+        this.#codigo = nuevoCodigo;
+    }
+
+    get nombre(){
+        return this.#nombre;
+    }
+    set nombre(nuevoNombre){
+        this.#nombre = nuevoNombre;
+    }
+
+    get categoria(){
+        return this.#categoria;
+    }
+    set categoria(nuevaCategoria){
+        this.#categoria = nuevaCategoria;
+    }
+
+    get precio(){
+        return this.#precio;
+    }
+    set precio(nuevoPrecio){
+        this.#precio = nuevoPrecio;
+    }
+
+    toJSON(){
+        return{
+            codigo: this.#codigo,
+            nombre: this.#nombre,
+            categoria: this.#categoria,
+            precio: this.#precio 
+        }
+    }
+}
